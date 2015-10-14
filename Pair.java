90628a3310fd5b370d8a1369c2ce98f3e0c2e52a
Eduardo Riojas Nacoud <eriojasn@gmail.com>
2015-10-14 09:59:19 -0500

    Update Pair.java

--- Pair.java
+++ Pair.java
@@ -13,13 +13,10 @@ public class Pair<L,R> implements IPair<L, R> {
 	  public R getRight() { return right; }
 
 	  @Override
-	  public int hashCode() { return left.hashCode() ^ right.hashCode(); }
-
-	  @Override
-	  public boolean equals(Object o) {
-	    if (!(o instanceof Pair)) return false;
-	    Pair pairo = (Pair) o;
-	    return this.left.equals(pairo.getLeft()) &&
-	           this.right.equals(pairo.getRight());
+	  public boolean equals(Object object) {
+	    if (!(object instanceof Pair)) return false;
+	    Pair pairObject = (Pair)oobject;
+	    return this.left.equals(pairObject.getLeft()) &&
+	           this.right.equals(pairObject.getRight());
 	  }
 }
